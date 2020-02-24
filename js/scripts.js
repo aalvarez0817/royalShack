af38e9c67c3f66c30f0ddb353056ab5ffb14190f
Angel Alvarez <alvarez0817@yahoo.com>
2020-02-23 18:14:24 -0600

    Royal Shack Draft Rev.A

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function() {
+
+    $("#reserveTable").click(function(){
+        $("#tableModal").modal("show");
+    });
+    
+    $("#checkVenue").click(function(){
+        $("#checkVenueModal").modal("show");
+    });
+    
+    $("#bookVenue").click(function(){
+        $("#bookVenueModal").modal("show");
+    });
+
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
